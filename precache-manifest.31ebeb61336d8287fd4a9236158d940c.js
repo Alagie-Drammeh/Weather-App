f75c414a9a204149b4136078f76fee55d174b680
Alagie Abdoulie DRammeh <aadrammeh@web.de>
2021-04-21 11:18:49 +0200

    Updates

---
+++ precache-manifest.31ebeb61336d8287fd4a9236158d940c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe7451dd833cad72afe258ad433c3ff",
+    "url": "./index.html"
+  },
+  {
+    "revision": "88ff6e91afb244249a3f",
+    "url": "./static/css/main.dc418fe2.chunk.css"
+  },
+  {
+    "revision": "df1e758f55f3dbc68706",
+    "url": "./static/js/2.31062568.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.31062568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88ff6e91afb244249a3f",
+    "url": "./static/js/main.5a0758e7.chunk.js"
+  },
+  {
+    "revision": "8cdb25f3ffb41440c4d8",
+    "url": "./static/js/runtime-main.e6c4d886.js"
+  }
+]);
